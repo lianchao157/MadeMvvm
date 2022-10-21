a9f2cbcdacda9ecb6ea927d86e7a1c9d9c32de1c
lianchao <18900945189@163.com>
2022-10-21 16:59:45 +0800

    2022.10.22 v2  添加顶部导航的子模块

---
+++ app/src/main/java/com/e/mademvvm/mvvmnews/ui/setting/SettingFragment.java
@@ -0,0 +1,35 @@
+package com.e.mademvvm.mvvmnews.ui.setting;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.e.mademvvm.R;
+import com.e.mademvvm.mvvmnews.ui.dashboard.DashboardViewModel;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+public   class SettingFragment  extends Fragment {
+    private SettingViewModel settingViewModel;
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        settingViewModel = ViewModelProviders.of(this).get(SettingViewModel.class);
+
+        View root = inflater.inflate(R.layout.fragment_setting, container, false);
+        final TextView textView = root.findViewById(R.id.text_setting);
+        settingViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
+}
